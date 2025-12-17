bdf3e94078a000c795a9147fb2f823a91b0690d6
Hasnainhanifse <hasnaindev786@gmail.com>
2025-12-17 15:34:05 +0500

    removed logging tag from android main activity

--- client/capacitor/android/app/src/main/java/org/outline/client/MainActivity.java
+++ client/capacitor/android/app/src/main/java/org/outline/client/MainActivity.java
@@ -1,13 +1,6 @@
 package org.outline.client;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-  private static final String TAG = "OutlineMainActivity";
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
 }
