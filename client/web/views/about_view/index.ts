814feca866482ccf7cf6d62865b0fc0237a8eee5
Muhammad Asad <asad@kodxsystem.com>
2026-05-22 13:10:58 +0500

    fix: move inset to root

--- client/web/views/about_view/index.ts
+++ client/web/views/about_view/index.ts
@@ -29,7 +29,6 @@ export class AboutView extends LitElement {
 
   static styles = css`
     :host {
-      --about-safe-area-top: 0px;
       display: flex;
       flex-direction: column;
       font-family: var(--outline-font-family);
@@ -53,19 +52,7 @@ export class AboutView extends LitElement {
 
     article {
       height: 100%;
-      padding: calc(32px + var(--about-safe-area-top)) 24px 0 24px;
-    }
-
-    @supports (padding-top: constant(safe-area-inset-top)) {
-      :host {
-        --about-safe-area-top: constant(safe-area-inset-top);
-      }
-    }
-
-    @supports (padding-top: env(safe-area-inset-top)) {
-      :host {
-        --about-safe-area-top: env(safe-area-inset-top);
-      }
+      padding: calc(32px + var(--outline-safe-area-top)) 24px 0 24px;
     }
 
     header img {
